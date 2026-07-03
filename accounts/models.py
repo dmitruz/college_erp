1594527ee2e2a1f19dc58b33d3e1c1f1dd42151b
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-07-03 23:44:34 +0300

    create model accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('faculty', 'Faculty'),
+        ('student', 'Student'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='student'
+    )
+
+    phone_number = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True
+    )
+
+    profile_picture = models.ImageField(
+        upload_to='profiles/',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.username
