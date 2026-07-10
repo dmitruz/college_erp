e38842886b2ee78a6a1623b10d007e950b708d3c
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-07-11 01:03:42 +0300

    refactor student model

--- students/models.py
+++ students/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from departments.models import Department
 
 
 class Student(models.Model):
@@ -15,8 +16,9 @@ class Student(models.Model):
     )
 
     student_id = models.CharField(
-        max_length=20,
-        unique=True
+         Department,
+        on_delete=models.PROTECT,
+        related_name="students"
     )
 
     date_of_birth = models.DateField()
