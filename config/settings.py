87c025ae298a794ec9d5b915b044b22064f03036
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-08-08 00:31:54 +0300

    Add JWT authentication to REST API

--- config/settings.py
+++ config/settings.py
@@ -153,6 +153,14 @@ AUTH_USER_MODEL = 'accounts.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
